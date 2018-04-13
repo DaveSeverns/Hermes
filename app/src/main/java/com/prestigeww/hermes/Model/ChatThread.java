8f6c86ac0237ac614be6d31d8f6a0986c6ff8d26
daveseverns <david.w.severns@gmail.com>
2018-04-12 22:29:46 -0400

    Firebase proxy added and ChatThread object created

--- app/src/main/java/com/prestigeww/hermes/Model/ChatThread.java
+++ app/src/main/java/com/prestigeww/hermes/Model/ChatThread.java
@@ -1,4 +1,91 @@
 package com.prestigeww.hermes.Model;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class ChatThread {
+    private String chatId;
+    private ArrayList<String> userIds;
+    private User admin;
+    private String chatName;
+    private ArrayList<MessageInChat> messages;
+
+    public ChatThread(){
+        chatId = "default";
+        userIds = new ArrayList<String>();
+        admin = null;
+        chatName = "Hermes Chat";
+        messages = new ArrayList<MessageInChat>();
+    }
+
+    public ChatThread(String chatId, User admin, String chatName){
+        this.chatId = chatId;
+        this.admin = admin;
+        this.chatName = chatName;
+        userIds = new ArrayList<>();
+        messages = new ArrayList<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChatThread)) return false;
+        ChatThread that = (ChatThread) o;
+        return Objects.equals(getChatId(), that.getChatId()) &&
+                Objects.equals(getUserIds(), that.getUserIds()) &&
+                Objects.equals(getAdmin(), that.getAdmin()) &&
+                Objects.equals(getChatName(), that.getChatName()) &&
+                Objects.equals(getMessages(), that.getMessages());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getChatId(), getUserIds(), getAdmin(), getChatName(), getMessages());
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public ArrayList<String> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(ArrayList<String> userIds) {
+        this.userIds = userIds;
+    }
+
+    public User getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(User admin) {
+        this.admin = admin;
+    }
+
+    public String getChatName() {
+        return chatName;
+    }
+
+    public void setChatName(String chatName) {
+        this.chatName = chatName;
+    }
+
+    public ArrayList<MessageInChat> getMessages() {
+        return messages;
+    }
+
+    public int addMessageToChatThread(MessageInChat messageToAdd){
+        messages.add(messageToAdd);
+        return messages.size();
+    }
+
+    public void setMessages(ArrayList<MessageInChat> messages) {
+        this.messages = messages;
+    }
 }
--- app/src/main/java/com/prestigeww/hermes/Utilities/FirebaseProxy.java
+++ app/src/main/java/com/prestigeww/hermes/Utilities/FirebaseProxy.java
@@ -1,4 +1,64 @@
 package com.prestigeww.hermes.Utilities;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+import com.prestigeww.hermes.Model.ChatThread;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+
 public class FirebaseProxy extends HermesUtiltity {
+    public FirebaseApp firebaseApp;
+    public FirebaseDatabase mFirebaseDatabase;
+    public DatabaseReference mUserDatabase;
+    public DatabaseReference mChatThreadDatabase;
+    public StorageReference mHermesStorage;
+    public FirebaseProxy(){
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mUserDatabase = mFirebaseDatabase.getReference().child("Users");
+        mChatThreadDatabase = mFirebaseDatabase.getReference().child("ChatThreads");
+    }
+
+    public ArrayList<ChatThread> getUsersChatsById(final ArrayList<String> chatIds){
+        final ArrayList<ChatThread> usersThreads = new ArrayList<ChatThread>();
+
+        while(usersThreads.isEmpty()){
+            mChatThreadDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (String id:chatIds){
+                        ChatThread tempThread = (ChatThread)dataSnapshot.child(id).getValue();
+                        usersThreads.add(tempThread);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.e("Database Error: ", databaseError.getDetails());
+                }
+            });
+        }
+
+
+        return usersThreads;
+    }
+
+
+    public String addNewThreadToFirebase(ChatThread chatThread){
+        String result = mChatThreadDatabase.push().setValue(chatThread).getResult().toString();
+        return result;
+    }
+
+
 }
