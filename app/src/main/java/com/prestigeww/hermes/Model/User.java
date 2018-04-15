e3ef76f24a6e0138dec59af9f141f04f86dcbc96
malik <malikvankirk@gmail.com>
2018-04-14 20:47:34 -0400

    waffles

--- app/src/main/java/com/prestigeww/hermes/Model/User.java
+++ app/src/main/java/com/prestigeww/hermes/Model/User.java
@@ -7,13 +7,25 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
-    }
+    }//end setUsername
 
     public void setId(long id) {
         this.id = id;
-    }
+    }//end setId
 
     public void setPassword(String password) {
         this.password = password;
-    }
-}
+    }// end setPassword
+
+    public String getPassword() {
+        return password;
+    }//end getPassword
+
+    public String getUsername() {
+        return username;
+    }//end getUsername
+
+    public long getId() {
+        return id;
+    }//end getId
+}//end USER
