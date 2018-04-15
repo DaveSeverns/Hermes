0e313127c5b12038a999cec980b61125350b1973
Krati <upadhyayrkk@gmail.com>
2018-04-15 17:12:01 -0400

    Working on UI

--- app/src/main/java/com/prestigeww/hermes/Activities/LoginActivity.java
+++ app/src/main/java/com/prestigeww/hermes/Activities/LoginActivity.java
@@ -1,15 +1,49 @@
 package com.prestigeww.hermes.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.prestigeww.hermes.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button signInButton;
+    Button createAccountButton;
+    EditText emailEditText;
+    EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signInButton = (Button) findViewById(R.id.signInButton);
+        createAccountButton = (Button) findViewById(R.id.createAccountButton);
+        emailEditText = (EditText) findViewById(R.id.loginEmailEditText);
+        passwordEditText = (EditText) findViewById(R.id.loginPasswordEditText);
+
+        //open chatThreadFeedActivity after button click
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ChatThreadFeedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //open SignUpActivity after button click
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SIgnUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
