41c79c9c05c66333d1141c8b38d137a196a6097e
Krati <upadhyayrkk@gmail.com>
2018-04-16 11:18:51 -0400

    Added ChatView UI

--- app/src/main/java/com/prestigeww/hermes/Activities/ChatWindowActivity.java
+++ app/src/main/java/com/prestigeww/hermes/Activities/ChatWindowActivity.java
@@ -4,12 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.prestigeww.hermes.R;
 
+import co.intentservice.chatui.ChatView;
+import co.intentservice.chatui.models.ChatMessage;
+
 
 public class ChatWindowActivity extends AppCompatActivity {
+
+    ChatView chatView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+
+        chatView = (ChatView) findViewById(R.id.chat_view);
+        chatView.setOnSentMessageListener(new ChatView.OnSentMessageListener() {
+            @Override
+            public boolean sendMessage(ChatMessage chatMessage) {
+                return true;
+            }
+        });
+
     }
 }
