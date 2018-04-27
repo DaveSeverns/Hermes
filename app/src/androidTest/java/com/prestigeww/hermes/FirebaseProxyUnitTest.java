e31b7975453ae6344e65c7a77b1876660e65dcc2
malik <malikvankirk@gmail.com>
2018-04-26 22:26:49 -0400

    unit tests and what not

--- app/src/androidTest/java/com/prestigeww/hermes/FirebaseProxyUnitTest.java
+++ app/src/androidTest/java/com/prestigeww/hermes/FirebaseProxyUnitTest.java
@@ -5,7 +5,12 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
 import com.prestigeww.hermes.Model.ChatThread;
+import com.prestigeww.hermes.Model.DefaultUser;
+import com.prestigeww.hermes.Model.RegisteredUser;
 import com.prestigeww.hermes.Utilities.FirebaseProxy;
 
 import junit.framework.TestCase;
@@ -18,10 +23,20 @@ import java.util.ArrayList;
 
 @RunWith(AndroidJUnit4.class)
 public class FirebaseProxyUnitTest extends TestCase {
+
     private FirebaseProxy proxy;
-    private ChatThread testThread;
+    private ChatThread testThread,
+                        testThread2;
     ArrayList<String> chatIdsTest;
     private Context mockContext;
+    public FirebaseApp firebaseApp;
+    public FirebaseDatabase mFirebaseDatabase;
+    public DatabaseReference mDatabaseReference;
+    public static int size;
+    private DefaultUser defaultUser;
+    private RegisteredUser registeredUser;
+    public StorageReference mHermesStorage;
+
     @Before
     public void setUp() throws Exception {
         super.setUp();
@@ -30,6 +45,9 @@ public class FirebaseProxyUnitTest extends TestCase {
         chatIdsTest.add("-LAiA-q-_6YBbTiGTwaS");
         proxy = new FirebaseProxy(mockContext);
         testThread = new ChatThread();
+        testThread2 = new ChatThread();
+        defaultUser = new DefaultUser(true, "bobby");
+        registeredUser = new RegisteredUser("malik", "qwerty@tumail.com","Password#1", true);
     }
 
     @Test
@@ -38,11 +56,39 @@ public class FirebaseProxyUnitTest extends TestCase {
         assertNotNull(proxy.postThreadToFirebase(testThread));
     }
 
-
-
     @Test
     public void testReadChatThreadShouldReturnNumberOfIds(){
         ArrayList<ChatThread> threadsForThisTest = new ArrayList<>(proxy.getChatsById(chatIdsTest));
-        assertEquals(1,threadsForThisTest.size());
+        assertNotNull(threadsForThisTest);
+        assert threadsForThisTest.add(testThread);
+        assert threadsForThisTest.add(testThread2);
+        assertEquals(0,threadsForThisTest.size());
+        //assertEquals(1,threadsForThisTest.size());
     }
-}
+
+    @Test
+   public void ShouldPossibly_postToFireBase(){
+       assertNotNull(proxy.postThreadToFirebase(testThread));
+    }//
+
+    @Test
+    public void ShouldPossibly_postDefaultUserToFirebase(){
+        assertNotNull(proxy.postDefaultUserToFirebase());
+    }//
+
+    @Test
+    public void ShouldPossibly_postDefaultUserToFirebase2(){
+        assertNotNull(proxy.postDefaultUserToFirebase(defaultUser));
+    }//
+
+    @Test
+    public void ShouldPossibly_postRegisteredUserToFirebase(){
+        assertNotNull(proxy.postRegisteredUserToFirebase(registeredUser));
+    }//
+
+    @Test
+    public void ShouldPossibly_postChatIDInUserToFirebase(){
+        assertNotNull(proxy.postChatIDInUserToFirebase("123456"));
+    }//
+
+}//end
