6d9f24115d67a6e897836e3fc062f0301252b524
daveseverns <david.w.severns@gmail.com>
2018-04-30 08:38:02 -0400

    ui changes to differentiate sender

--- app/src/main/java/com/prestigeww/hermes/Activities/ChatWindowActivity.java
+++ app/src/main/java/com/prestigeww/hermes/Activities/ChatWindowActivity.java
@@ -125,6 +125,7 @@ public class ChatWindowActivity extends AppCompatActivity implements NfcAdapter.
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                messagesList.clear();
                 for(DataSnapshot messages:
                         dataSnapshot.getChildren()){
                     MessageInChat tempMessage;
@@ -332,5 +333,11 @@ public class ChatWindowActivity extends AppCompatActivity implements NfcAdapter.
             }
 
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent backIntent = new Intent(ChatWindowActivity.this, ChatThreadFeedActivity.class);
+        startActivity(backIntent);
     }
 }
